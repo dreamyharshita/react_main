d5fde57aef0d551e8c22e96d06ab5bd020738b18
dreamyharshita <dreamjob9893@gmail.com>
2023-12-08 12:39:33 +0530

    Delete button// Dom manipulation

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,10 +1,12 @@
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseDetails=   (props) =>{
 
     return(<div className='expense-item__description'>
+
 <h2 className='expense-item h2'>{props.title}</h2>
-<div className='expense-item__description'>{props.location}</div>
+<div className='expense-item__location'>{props.location}</div>
 <div className='expense-item__price'>${props.amount}</div>
 
 
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,13 +5,17 @@ import './ExpenseItem.css';
 
 const ExpenseItem= (props) => {
   
-
     return (       
-        <div className="expense-item">
+        <div className="expense-item" id="main">
        <ExpenseDate date={props.date}/>
         <ExpenseDetails title={props.title} location={props.location} amount={props.amount}/>
         
-
+<button onClick={(e)=>{
+  alert("deleting");
+{ let item = e.target.parentElement;
+  item.remove() }
+}
+}>DELETE</button>
         </div>
       );
 }
