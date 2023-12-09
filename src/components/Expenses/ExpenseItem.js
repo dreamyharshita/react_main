68ee8d8de6441c47184d7004317a89e5b123e4ba
dreamyharshita <dreamjob9893@gmail.com>
2023-12-09 13:07:25 +0530

    Manipulating Dom// Using state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,39 @@
+import React,{useState} from 'react';
+
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css';
 
+
 const ExpenseItem= (props) => {
-  
+  const[title,setTitle]= useState(props.title);
+  const[amount,setExpense]= useState(props.amount);
     return (       
-        <div className="expense-item" id="main">
+        <Card className="expense-item" >
        <ExpenseDate date={props.date}/>
-        <ExpenseDetails title={props.title} location={props.location} amount={props.amount}/>
+        <ExpenseDetails title={title} location={props.location} amount={amount}/>
+                
+<button onClick={()=>{
+  setTitle('Updated!');
+  console.log(title);
+}
+}>Change Title</button>
+<button onClick={()=>{
+  setExpense('100');
+  console.log(amount);
+}
+}>Change Expense</button>
         
 <button onClick={(e)=>{
   alert("deleting");
-{ let item = e.target.parentElement;
+{ 
+  let item = e.target.parentElement;
   item.remove() }
 }
 }>DELETE</button>
-        </div>
+        </Card>
+        
       );
 }
 
