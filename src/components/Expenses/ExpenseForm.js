519e638def0569aa301d2d16e1e492dc46a77554
dreamyharshita <dreamjob9893@gmail.com>
2023-12-11 13:10:51 +0530

    Multiple states in expense form

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,24 +1,44 @@
+import  React,{ useState } from 'react';
+import ExpenseItem from './ExpenseItem';
+import expenses from './Expenses';
 
+let title,amount,date,location;
 const ExpenseForm= () =>{
+    const [title,setTitle]=useState('');
+    const [amount,setAmount]=useState('');
+    const [date,setDate]=useState('');
+    const [location,setLocation]=useState('');
     const TitleHandler=(e)=>{
-       console.log(e.target.value);
+        setTitle(e.target.value); 
+       console.log(title);
     }
     const DateHandler=(e)=>{
-        console.log(e.target.value);
+        setDate(e.target.value);
+        console.log(date);
      }
      const AmountHandler=(e)=>{
-        console.log(e.target.value);
+        setAmount(e.target.value);
+        console.log(amount);
      }
-
+     const LocationHandler=(e)=>{
+        setLocation(e.target.value);
+        console.log(location);
+     }
+    
+  const AddExpense= () => {
+    console.log("Expense added");
+  }
     return(<div>
         <form>
         <label>Expense Title</label>
         <input type="Text" onChange={TitleHandler}></input>
+        <label>Expense Location</label>
+        <input type="Text" onChange={LocationHandler}></input>
         <label>Expense Amount</label>
         <input type="Number" min="0.01" step="0.01" onChange={AmountHandler}></input>
         <label>Date</label>
         <input type="date" onChange={DateHandler} ></input>
-        <button >Add Expense</button>
+        <button onClick={AddExpense}>Add Expense</button>
         </form>
     </div>
 
