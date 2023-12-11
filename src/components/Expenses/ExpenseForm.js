c0cfb3850513372ccb948dbf85424c5b797946de
dreamyharshita <dreamjob9893@gmail.com>
2023-12-11 14:25:19 +0530

    Logging entered expense data

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -4,17 +4,17 @@ import expenses from './Expenses';
 
 let title,amount,date,location;
 const ExpenseForm= () =>{
-   // const [title,setTitle]=useState('');
-   // const [amount,setAmount]=useState('');
-   // const [date,setDate]=useState('');
-   // const [location,setLocation]=useState('');
-   const [userInput,setUserInput]=useState({
+    const [title,setTitle]=useState('');
+    const [amount,setAmount]=useState('');
+    const [date,setDate]=useState('');
+   const [location,setLocation]=useState('');
+  /* const [userInput,setUserInput]=useState({
       title:'',
       date:'',
       location:'',
       amount:''
-   })
-    const TitleHandler=(e)=>{
+   })*/
+    /*const TitleHandler=(e)=>{
         setUserInput((prevState)=> {
         return  {...prevState,title: e.target.value};
       }); 
@@ -34,22 +34,54 @@ const ExpenseForm= () =>{
       setUserInput((prevState)=> {
          return  {...prevState,location: e.target.value};
        }); 
-     }
+     }*/
+     const TitleHandler=(e)=>{
+      console.log(e.target.value);
+       setTitle(e.target.value); 
+      console.log(title);
+   }
+   const DateHandler=(e)=>{
+       console.log(e.target.value);
+       setDate(e.target.value);
+       console.log(date);
+    }
+    const AmountHandler=(e)=>{
+       console.log(e.target.value);
+       setAmount(e.target.value);
+       console.log(amount);
+    }
+
+    const LocationHandler=(e)=>{
+       setLocation(e.target.value);
+       console.log(location);
+    }
     
-  const AddExpense= () => {
-    console.log("Expense added");
-  }
+  const SubmitHandler= (event) => {
+event.preventDefault();
+    
+    const expenseData={
+      title:title,
+      date:new Date(date),
+      amount:amount,
+     location:location
+    }
+    
+    console.log(expenseData);
+   
+  };
     return(<div>
-        <form>
+        <form onSubmit={SubmitHandler}>
         <label>Expense Title</label>
         <input type="Text" onChange={TitleHandler}></input>
         <label>Expense Location</label>
         <input type="Text" onChange={LocationHandler}></input>
+        <br/>
         <label>Expense Amount</label>
         <input type="Number" min="0.01" step="0.01" onChange={AmountHandler}></input>
         <label>Date</label>
         <input type="date" onChange={DateHandler} ></input>
-        <button onClick={AddExpense}>Add Expense</button>
+        <br/>
+        <button type="submit" >Add Expense</button>
         </form>
     </div>
 
