f259049aebcbec64960c596dbc82d95b9a0a8847
dreamyharshita <dreamjob9893@gmail.com>
2023-12-14 14:47:47 +0530

    Adding non working filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,17 @@
 
-
-import React from "react";
+import React,{useState} from 'react';
 import ExpenseItem from "./ExpenseItem";
-import Card from '../UI/Card';
 
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 const Expenses= (props)=>{
+    const [filteredyear,setFilteredYear]=useState('2020');
+    const filterHandler= selectedYear =>{
+        setFilteredYear(selectedYear);
+    }
     return (
         <Card classname="expenses">
+            <ExpensesFilter selected={filteredyear}  onChangeFilter={filterHandler}></ExpensesFilter>
             {
                 props.item.map(
                     expense=>(
