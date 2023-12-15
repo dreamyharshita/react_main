29ccf52edf3718ceb932dcb32528c12aefb1cd88
dreamyharshita <dreamjob9893@gmail.com>
2023-12-15 11:32:11 +0530

    Filter conditional rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,7 +5,7 @@ import ExpenseItem from "./ExpenseItem";
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 const Expenses= (props)=>{
-    const [filteredyear,setFilteredYear]=useState('2020');
+    const [filteredyear,setFilteredYear]=useState('2023');
     
     const filterHandler= selectedYear =>{
        
@@ -16,13 +16,11 @@ const Expenses= (props)=>{
         const result=props.item.filter(exp=>{
             return exp.date.getFullYear().toString()===filteredyear;
              });
-           
-    return (
-        <Card classname="expenses">
-            <ExpensesFilter selected={filteredyear}  onChangeFilter={filterHandler}></ExpensesFilter>
-
-            {
-                result.map(
+           let contentmorethan1="";
+             let defaultcontent= <p>No data found</p>;
+                if(result.length>0){
+                    
+                defaultcontent=result.map(
 
                     expense=>(
                         <ExpenseItem 
@@ -32,10 +30,33 @@ const Expenses= (props)=>{
                         date={expense.date} 
                         ></ExpenseItem>
                     )
-                )}
-                
+                );
+                    }
+                    if(result.length===1){
+                         contentmorethan1=<p>Only single Expense here. Please add more..."</p>;
+                    }
+               
+    return (
+        <Card classname="expenses">
+            <ExpensesFilter selected={filteredyear}  onChangeFilter={filterHandler}></ExpensesFilter>
+           {defaultcontent}
+                {contentmorethan1}
             
         </Card>
     )
+
 }
 export default Expenses;
+ /*{result.length===0 && <p>No data found</p>}
+            {result.length>0 && result.map(
+
+                    expense=>(
+                        <ExpenseItem 
+                        key={expense.id}
+                        title={expense.title} 
+                        amount={expense.amount} 
+                        date={expense.date} 
+                        ></ExpenseItem>
+                    )
+                ) }
+          */
